581ca4974a317b66ecbc56af4bb6268ea7335dd2
alim-2023 <alim@crystallitedigital.com>
2023-03-28 21:47:04 +0500

    some fixesv.5

--- components/Finetoothedbook.js
+++ components/Finetoothedbook.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '@/styles/finetoothedbook.module.css'
+import styles from '@/styles/Finetoothedbook.module.css'
 import { Container,Row,Col } from 'react-bootstrap'
 import Link from 'next/link'
 
