76cca53942e3a7ecc09e389fbe806eee6ec2346b
alim-2023 <alim@crystallitedigital.com>
2023-03-28 21:46:05 +0500

    some fixesv.4

--- components/Stillonthefence.js
+++ components/Stillonthefence.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '@/styles/Stillonthefence.module.css'
+import styles from '@/styles/stillonthefence.module.css'
 import { Container,Row,Col } from 'react-bootstrap'
 
 
