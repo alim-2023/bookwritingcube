5538f51d6e6e9fb1eb519507034b09525aeecfda
alim-2023 <alim@crystallitedigital.com>
2023-04-05 23:55:56 +0500

    press page fixes

--- src/pages/press-release-book-writing-cube.js
+++ src/pages/press-release-book-writing-cube.js
@@ -127,9 +127,9 @@ const Announcement = () => {
                         </li>
                         <li className='font-f'><strong>Zip code: </strong> 97008
                         </li>
-                        <li className='font-f'><strong>Email: </strong> info@bookwritingcube.com
+                        <li className='font-f'><strong>Email: </strong> <Link className='color-black font-f textdocationnone' href= "mailto:info@bookwritingcube.com">info@bookwritingcube.com</Link>
                         </li>
-                        <li className='font-f'><strong>Phone: </strong> (302) 883-8877
+                        <li className='font-f'><strong>Phone: </strong> <Link className='color-black font-f textdocationnone' href="tel:+1302 883-8877">(302) 883-8877</Link>
                         </li>
                         <li className='font-f'><strong>Working hours: </strong> Mon-Fri, 9:00 AM - 5:00 PM
                         </li>
